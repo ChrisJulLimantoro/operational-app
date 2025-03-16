be234e30bb96bf53002846d2944d25f3e2e99788
ChrisJulLimantoro <christopherlimantoro@gmail.com>
2025-03-16 13:36:29 +0700

    feat: done stock_opname add

--- lib/api/stock_opname.dart
+++ lib/api/stock_opname.dart
@@ -1,6 +1,5 @@
 import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
-import 'package:go_router/go_router.dart';
 import 'package:operational_app/helper/api.dart';
 import 'package:operational_app/helper/notification.dart';
 import 'package:operational_app/model/stock_opname.dart';
